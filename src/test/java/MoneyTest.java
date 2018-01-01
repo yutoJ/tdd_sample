b0b50e1eb492d4e76143825069b4738e91e6bab6
yuto <yutoogata4@gmail.com>
2018-01-01 16:30:56 +0900

    close #1 #4

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -11,8 +11,10 @@ public class MoneyTest {
     @Test
     public void MultiplicationTest() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
 
     }
 }

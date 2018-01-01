af9cd5c3d13ba4aad9a97e0278656e43797c7159
yuto <yutoogata4@gmail.com>
2018-01-01 17:43:46 +0900

    1st cycle #6

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -9,5 +9,10 @@ public class Dollar {
     public Dollar times(int multiplier){
         return new Dollar(this.amount * multiplier);
     }
+
+    public boolean equals(Object object){
+        Dollar dollar = (Dollar) object;
+        return this.amount == dollar.amount;
+    }
 }
 
--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -3,6 +3,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 
 @RunWith(JUnit4.class) // テストランナーを指定
@@ -16,5 +18,11 @@ public class MoneyTest {
         product = five.times(3);
         assertEquals(15, product.amount);
 
+    }
+
+    @Test
+    public void EqualityTest() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }

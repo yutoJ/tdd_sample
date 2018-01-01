e22ad64aee30c757f68a9c6bfce25b229ca58440
yuto <yutoogata4@gmail.com>
2018-01-01 16:21:53 +0900

    chapter 1 ends

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -3,10 +3,11 @@ public class Dollar {
     int amount;
 
     public Dollar(int amount) {
+        this.amount = amount;
     }
 
     public void times(int multiplier){
-        amount = 5 * 2;
+        this.amount *= multiplier;
     }
 }
 

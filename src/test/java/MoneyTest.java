2f0603f3ee7ac42f56390bd9ed2a031ca9723fa5
yuto <yutoogata4@gmail.com>
2018-01-02 15:47:39 +0900

    close #9

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -22,26 +22,12 @@ public class MoneyTest {
     public void EqualityTest() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
-    }
-
-    @Test
-    public void FrancMultiplicationTest() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
     public void CurrencyTest() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    public void MoneyTest() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
 }

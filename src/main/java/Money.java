578055fc783ec8ced36618564151ab413e6879ea
yuto <yutoogata4@gmail.com>
2018-01-02 13:48:18 +0900

    chapter 8 ends

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,10 +1,19 @@
-public class Money {
+abstract class Money {
 
     protected int amount;
 
+    abstract Money times(int multiplier);
 
     public boolean equals(Object object){
         Money money = (Money) object;
         return this.amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
     }
 }

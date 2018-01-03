6d79a350ad4a4dc39532710fd5fe099b67052e88
yuto <yutoogata4@gmail.com>
2018-01-03 11:42:14 +0900

    close #18

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,14 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+
+    private Map<Pair, Integer> rates = new HashMap<Pair, Integer>();
 
     public Money reduce(Expression source, String to){
         return source.reduce(this, to);
     }
 
     void addRate(String from, String to, int rate) {
-
+        rates.put(new Pair(from, to), rate);
     }
 
     int rate(String from, String to) {
-        return (from.equals("CHF") && to.equals("USD") ? 2 : 1);
+        if (from.equals(to)) return 1;
+        return rates.get(new Pair(from, to));
     }
 }

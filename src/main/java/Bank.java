9a73200d1a29a6486e0d58f483eb231a7dea59c4
yuto <yutoogata4@gmail.com>
2018-01-03 11:16:06 +0900

    2nd cycle: #18

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,10 +1,14 @@
 public class Bank {
 
     public Money reduce(Expression source, String to){
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
 
     void addRate(String from, String to, int rate) {
 
+    }
+
+    int rate(String from, String to) {
+        return (from.equals("CHF") && to.equals("USD") ? 2 : 1);
     }
 }
